3e87c854c99a55ff00c0a76ff01ccf3ce21b4373
Glen Noworyta <garthalgar2598@gmail.com>
2020-04-24 13:07:59 -0400

    db updates 4-24-20

--- ramrod/settings.py
+++ ramrod/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import dj_database_url
+import decouple
 from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
